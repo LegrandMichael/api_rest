4fd4de14b8d85525cdb4d2dc568189bbb06408cc
Michaël Legrand <michael.c.legrand@gmail.com>
2018-03-01 12:08:03 +0100

    Add mangement error 404

--- src/AppBundle/Controller/TaskController.php
+++ src/AppBundle/Controller/TaskController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Entity\Task;
 
 class TaskController extends Controller
@@ -41,6 +42,38 @@ class TaskController extends Controller
                 'state' =>$task->getState()
             ];
         }
+        return new JsonResponse($formated);
+    }
+
+    /**
+     * @Route("/tasks/{task_id}", name="tasks_one")
+     * @Method({"GET"})
+    */
+    public function getTaskAction(Request $request)
+    {
+        $task = $this->get('doctrine.orm.entity_manager')
+            ->getRepository('AppBundle:Task')
+            ->find($request->get('task_id'));
+        /* @var $task Task */
+
+        if (empty($task)) {
+            return new JsonResponse(['message' => 'user not found'], Response::HTTP_NOT_FOUND);
+        }
+        
+        $formated = [
+            'id' => $task->getId(),
+            'date of receipt' => $task->getDateReceipt(), 
+            'user concerned' => 'user concerned',
+            'theme' => $task->getTheme(),
+            'priority' => $task->getPriorityLevel(),
+            'deadline' => $task->getDeadline(),
+            'public concerned' => $task->getPublicConcerned(),
+            'goal' => $task->getGoal(),
+            'broadcasting' => $task->getBroadcasting(),
+            'answer' => $task->getAnswer(),
+            'treated by' => 'treated by',
+            'state' =>$task->getState()
+        ];
         return new JsonResponse($formated);
     }
 }
--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Entity\User;
 
 class UserController extends Controller
@@ -34,6 +35,33 @@ class UserController extends Controller
                 'organization' => $user->getOrganization()
             ];
         }
+        return new JsonResponse($formated);
+    }
+
+    /**
+     * @Route("/users/{user_id}", name="users_one") 
+     * @Method({"GET"})
+    */
+    public function getUserAction(Request $request)
+    {
+        $user = $this->get("doctrine.orm.entity_manager")
+            ->getRepository("AppBundle:User")
+            ->find($request->get("user_id"));
+        /* @var $user User */
+
+        if (empty($place)) {
+            return new JsonResponse(['message' => 'user not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $formated = [
+            'first name' => $user->getFirstName(),
+            'last name' => $user->getLastName(),
+            'function' => $user->getFunction(),
+            'phone number' => $user->getPhoneNumber(),
+            'cellphone' => $user->getCellphone(),
+            'email address' => $user->getEmailAddress(),
+            'organization' => $user->getOrganization()
+        ];
         return new JsonResponse($formated);
     }
 }
